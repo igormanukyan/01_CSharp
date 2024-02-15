a462115799dafde5d48faaae33470cd41a89b092
Igor Manukyan <igormanukyan@mail.com>
2024-02-15 17:07:08 +0300

    операции с разными типами данных

---
+++ Lesson_1/Task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+System.Console.WriteLine(sum);
+
+long l1 = 6;
+long sum2 = i1 + l1;
+System.Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = i1 + d1;
+System.Console.WriteLine(sum3);
+
+int product = i1 * i2;
+double product2 = i1 * i2;
+System.Console.WriteLine(product);
+System.Console.WriteLine(product2);
+
+int quotient = i1 / i2;
+System.Console.WriteLine(quotient);
+
+d1 = 6;
+double quotient2 = i1 / d1;
+System.Console.WriteLine(quotient2);
