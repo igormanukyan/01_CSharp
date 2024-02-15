cfbf76836d86d89d0644b7fb67ac5e5911c18196
Igor Manukyan <igormanukyan@mail.com>
2024-02-15 21:43:27 +0300

    Решение задачи в массиве на нахождение элементов

---
+++ Lesson_2/Task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿int N = 10;
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int i = 0;
+
+while (i < N)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+
+    }
+    i = i + 1;
+}
