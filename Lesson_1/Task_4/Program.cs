ba71a783ea8ae9857fff2c31cc51c4e6f525071c
Igor Manukyan <igormanukyan@mail.com>
2024-02-15 18:39:59 +0300

    Решение задачи с гирями

---
+++ Lesson_1/Task_4/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 23;
+int b = 4;
+int c = 63;
+int d = 62;
+int e = 61;
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+System.Console.Write("Maximum is: ");
+System.Console.WriteLine(max);
