9626ed060db818cfdf06176677f7ffaf684c85d3
Igor Manukyan <igormanukyan@mail.com>
2024-02-15 21:52:34 +0300

    Решение задач на нахождение максимального значения

---
+++ Lesson_2/Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿int N = 10;
+int [] array = { 1,4,5,3,66,33,6,5,60,23};
+int i = 0;
+int max = array[0];
+
+while (i < N)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+System.Console.WriteLine(max);
