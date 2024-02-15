d17eaf11ab0073e82d5b70582b03fffc28c625ee
Igor Manukyan <igormanukyan@mail.com>
2024-02-15 19:51:38 +0300

    добавил урок 2, массивы

---
+++ Lesson_2/Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = new int [5];
+
+arr [0] = 5;
+arr [1] = 7;
+arr [2] = 3;
+arr [3] = 8;
+arr [4] = 1;
+
+System.Console.WriteLine(arr[0]);
+System.Console.WriteLine(arr[1]);
+System.Console.WriteLine(arr[2]);
+System.Console.WriteLine(arr[3]);
+System.Console.WriteLine(arr[4]);
+
+int [] arr2 = new int [5] {1,2,3,4,5};//полный формат создания и заполнения
+int [] arr3 = {1,2,3,4,5};// упрощенный формат создания массива
